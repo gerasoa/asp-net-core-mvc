f8889ed874b164eba9cd18c1d25985c42bb9e10c
gerasoa <gerasoa@gmail.com>
2023-11-13 10:32:24 +0000

    Areas finish

---
+++ MySolution/src/MyProject/Modules/Products/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Areas.Products.Controllers
+{
+    [Area("Products")]
+    public class ManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MySolution/src/MyProject/Program.cs
+++ MySolution/src/MyProject/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.EntityFrameworkCore;
 using MyProject.Data;
 
@@ -5,6 +7,15 @@ using MyProject.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modules/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modules/{2}/Views/Shared/{0].cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
+
 
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
@@ -15,6 +26,10 @@ var app = builder.Build();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.MapControllerRoute(
+    name: "Areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
 app.MapControllerRoute(
     name: "default",
