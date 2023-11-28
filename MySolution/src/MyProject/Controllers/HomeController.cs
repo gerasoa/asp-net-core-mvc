228109eabcbbc997257956840358ca53a4380ba3
gerasoa <gerasoa@gmail.com>
2023-11-28 22:07:34 +0000

    configFile

--- MySolution/src/MyProject/Controllers/HomeController.cs
+++ MySolution/src/MyProject/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MyProject.Configuration;
 
 namespace MyProject.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApiConfiguration ApiConfig;
+
+        public HomeController(IOptions<ApiConfiguration> apiConfiguration)
+        {
+            ApiConfig = apiConfiguration.Value;            
+        }
+
         public IActionResult Index()
         {
             var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var user = ApiConfig.UserKey;
+            var secret = ApiConfig.UserSecret;
+            var domain = ApiConfig.Domain;
+
 
             return View();
         }
