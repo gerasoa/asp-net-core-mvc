b25fa2086af2ba2c80432323e2d700f01924a0bc
gerasoa <gerasoa@gmail.com>
2023-12-04 17:50:39 +0000

    finish

--- MySolution/src/MyProject/Controllers/HomeController.cs
+++ MySolution/src/MyProject/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using MyProject.Configuration;
+using MyProject.Models;
 
 namespace MyProject.Controllers
 {
@@ -10,7 +11,7 @@ namespace MyProject.Controllers
 
         public HomeController(IOptions<ApiConfiguration> apiConfiguration)
         {
-            ApiConfig = apiConfiguration.Value;            
+            ApiConfig = apiConfiguration.Value;
         }
 
         public IActionResult Index()
@@ -23,6 +24,45 @@ namespace MyProject.Controllers
 
 
             return View();
+        }
+
+        [Route("test")]
+        public IActionResult Test()
+        {
+            throw new Exception("Something is wrong!");
+
+            return View("Index");
+        }
+
+        [Route("error/{id:length(3,3)}")]
+        public IActionResult Error(int id)
+        {
+            var modelError = new ErrorViewModel();
+
+            if(id == 500)
+            {
+                modelError.Message = "An error has occurred! Please try again later or contact our support.";
+                modelError.Title = "An error has occurred!";
+                modelError.ErrorCode = id;
+            }
+            else if(id ==404)
+            {
+                modelError.Message = "The page you are looking for does not exist! <br />If you have any questions contact our support";
+                modelError.Title = "Page not found";
+                modelError.ErrorCode = id;
+            }
+            else if (id ==403)
+            {
+                modelError.Message = "You’re not allowed to do this.";
+                modelError.Title = "access denied";
+                modelError.ErrorCode = id;
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+
+            return View("Error", modelError);
         }
     }
 }
